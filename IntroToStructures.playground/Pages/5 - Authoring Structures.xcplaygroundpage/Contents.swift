219b2f7546fa00b496684b080284cb49f858a6fc
Ilana Mohamed <Imohamed@lcs.on.ca>
2020-03-05 13:34:50 -0500

    Finished writing and invoking structure for circle

--- IntroToStructures.playground/Pages/5 - Authoring Structures.xcplaygroundpage/Contents.swift
+++ IntroToStructures.playground/Pages/5 - Authoring Structures.xcplaygroundpage/Contents.swift
@@ -32,7 +32,57 @@ import Foundation
  
  */
 // Begin your work here...
+struct Circle {
+    
+    //MARK: Properties
+    var radius: Double
+    
+    //Computed property to return area
+    var area: Double {
+        return Double.pi * pow(radius, 2.0)
+    }
+    
+    //Computed property to return circumference
+    var circumference: Double {
+        return 2 * Double.pi * radius
+    }
+    
+    //Computed property to return description of shape
+    var circleDescription: String {
+        return "The area of this circle is " + String(format: "%.1f", area) + " square units and the circumference of this circle is " + String(format: "%.1f", circumference) + " units"
+    }
+}
 
+    // Create a circle instance
+    var someCircle = Circle(radius: 3)
+
+    // What is the area?
+    someCircle.area
+
+    // Report on the area?
+    print(someCircle.circleDescription)
+
+
+// Create a structure for a triangle
+struct Triangle {
+    // MARK: Properties
+    var base: Double
+    var height: Double
+    var sideA: Double
+    var sideC: Double
+    
+    //Computed Property to Return area
+    var area: Double {
+        return 1/2 * base * height
+    }
+    
+    //Computed property to return perimeter
+    var perimeter: Double {
+        return sideA + base + sideC
+    }
+    
+    
+}
 
 
 /*:
